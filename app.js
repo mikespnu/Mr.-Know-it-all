d19ba00ec9d98083c243c2ddff5e460b7e7b443f
Mikespnu <mikespnu@gmail.com>
2023-01-09 18:23:50 -0500

    Added loader. Improved script. Removed API keys and implemented a web proxy to complete teh request

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 var inputel = document.querySelector('#query');
 
 var queryBuilder = function () {
-  let value = inputel.value
+  let value = inputel.value;
   return `{"model": "text-davinci-003", "prompt": "${value}", "temperature": 0, "max_tokens": 400}`
 }
 
 
 document.querySelector('#send').addEventListener('click', () => {
+
+  //create loading element
+  let image =  document.createElement("img");
+  image.setAttribute("src", "assets/ellipsis-loader.gif");
+  image.setAttribute("width", 35);
+  image.classList = "loading-image";
+
+  //add element to dom
+  document.querySelector("#element").appendChild(image);
+
+
     //Create AJAX Object
     var req = new XMLHttpRequest();
 
     //Open Request
-    req.open('POST', 'https://api.openai.com/v1/completions');
+    req.open('POST', 'https://thetvmounters.com/api/test.php');
 
     //CallBack
     req.onreadystatechange = function () {
       if (req.readyState === 4) {
 
-      //Parse text
+      // //Parse text
       let data = JSON.parse(req.responseText)
-      console.log(data)
+      // console.log(data)
       let text = data['choices'][0].text
-
-      //Filter Text
+      // //Filter Text
       text = text.replace(/\n/gm, '')
 
       setTimeout(function () {
+        image.remove();
         const instance = new TypeIt('#element', {
           speed: 50
         })
@@ -41,14 +52,9 @@ document.querySelector('#send').addEventListener('click', () => {
     }
   }
 
-  req.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
-
-  req.setRequestHeader(
-    'Authorization',
-    'Bearer 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'
-  )
+  req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8')
 
   let test = queryBuilder()
-  req.send(test)
+  req.send(`data=${test}`);
   inputel.value = ''
 })
